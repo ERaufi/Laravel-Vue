6a0a5ebb689d5c04ca6617dd24bbef7fc3331e87
ERaufi <79995615+ERaufi@users.noreply.github.com>
2023-05-22 01:03:25 +0300

    Lesson 1 Laravel and Vue Installation

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    @vite(['resources/js/app.js', 'resources/sass/app.scss'])
+</head>
+
+<body>
+    <div id="app">
+        <example-component></example-component>
+    </div>
+</body>
+
+</html>
